6dd94b52fb8aa5d4158b0706bf5cc2b8465ccfe8
Tomaz20 <xubuntu@xubuntu-VirtualBox.(none)>
2016-06-15 13:31:51 +0100

    fix

--- JobScript/getTempos.pl
+++ JobScript/getTempos.pl
@@ -6,7 +6,7 @@ use utf8::all;
 
 my ($dirname, $filename);
 
-open my $out, "tempos.txt" or die "Could not open $filename: $!";
+open my $out, ">","tempos.txt" or die "Could not open : $!";
 
 $dirname = $ARGV[0];
 opendir(DIR, $dirname) or die "Could not open $dirname\n";
@@ -18,9 +18,10 @@ while ($filename = readdir(DIR)) {
 	open my $fh, "$dirname/$filename" or die "Could not open $filename: $!";
 
 	while (<$fh>){
-		if(/Rendering: \[[\+ ]+\]  \(([0-9.]+s)\)/);
+		if(/Rendering: \[[\+ ]+\]  \(([0-9.]+s)\)/g){
 
-		print $out "$filename: $1\n";
+			print $out "$filename: $1\n";
+		}
 	}
 	close $fh;
 }
